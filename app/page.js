e93148441316992431f822a55bbe47b3dfd7593c
Jennifer Tan <jennifermaria.tan@gmail.com>
2024-05-10 19:20:18 +1000

    Fix redundant class pageHeader and add information to the home page.

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="aboutUsContainer">
       <div className="contentContainer">
-        <h1 className="pageHeader"> Welcome to UniTrack! </h1>
+        <h1 className="homePageHeader"> Welcome to UniTrack! </h1>
         <div className="body">
           <p> Unitrack features an easy and simple way to plan your degree at The University of Sydney. Click on <em>Degree Plan</em> to get started! </p>
         </div>
@@ -16,9 +16,9 @@ export default function Home() {
         <div className="body">
           <p> 
             Unitrack first came to life as a project submission for <a href="https://www.sydney.edu.au/engineering/industry-and-community/partner-with-us/coding-fest.html">Coding Fest 2023</a> by
-            4 undergraduate students. One of the toughest challenges we encountered upon starting university was navigating degree planning with multiple websites and resources
-            provided by the university. Degree planning is not only crucial for students to graduate with correct requirements, but also to conveniently and efficiently plan out semesters.
-            Hence, we thought, <em>what is more exciting than building our own degree planner?</em>
+            4 undergraduate students, which won the Coding Fest 2023 Citadel Securities Best UG Junior Project Award Champion. As undergraduates, one of the toughest challenges we encountered
+            upon starting university was navigating degree planning with multiple websites and resources. Hence, we decided to create our own degree planner to streamline the process of degree
+            planing for future USYD students!
           </p>
         </div>
       </div>

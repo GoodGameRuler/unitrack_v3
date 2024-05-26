c34c1f95a723a567454590c53450ac3107d2fe18
Jennifer Tan <jennifermaria.tan@gmail.com>
2024-05-26 11:39:09 +1000

    Fix sizing issue on mozilla and chrome and refined content

--- app/page.js
+++ app/page.js
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <div className="aboutUsContainer">
       <div className="contentContainer">
-        <h1 className="homePageHeader"> Welcome to UniTrack! </h1>
-        <div className="body">
-          <p> Unitrack features an easy and simple way to plan your degree at The University of Sydney. Click on <em>Degree Plan</em> to get started! </p>
+        <h1 className="heading"> Welcome to UniTrack! </h1>
+        <div className="aboutUsTagline">
+          <p> Easy and simple way to plan your degree at The University of Sydney.</p>
         </div>
       </div>
       <Divider />
       
       <div className="contentContainer">
         <h2 className="subheading"> Our Story </h2>
-        <div className="body">
+        <div className="body aboutUs">
           <p> 
             Unitrack first came to life as a project submission for <a href="https://www.sydney.edu.au/engineering/industry-and-community/partner-with-us/coding-fest.html">Coding Fest 2023</a> by
-            4 undergraduate students, which won the Coding Fest 2023 Citadel Securities Best UG Junior Project Award Champion. As undergraduates, one of the toughest challenges we encountered
-            upon starting university was navigating degree planning with multiple websites and resources. Hence, we decided to create our own degree planner to streamline the process of degree
-            planing for future USYD students!
+            4 undergraduate students: Udit Samant, Jennifer Tan, Parth Bhargava, Devanshi Mirchandani. It won the Coding Fest 2023 Citadel Securities Best UG Junior Project Award Champion.
+            As undergraduates, one of the toughest challenges we encountered upon starting university was navigating degree planning with multiple websites and resources. Hence, we decided to create
+            our own degree planner to streamline the process of degree planning for future USYD students!
           </p>
         </div>
       </div>
@@ -42,39 +42,8 @@ export default function Home() {
       <Divider />
 
       <div className="contentContainer">
-        <h2 className="subheading"> Contributors </h2>
-        <div className="profileContainer">
-            <ProfileLink
-              key={'jennifermtan'}
-              username={'jennifermtan'}
-              imageUrl={'/images/jenny.png'}
-            />
-            <ProfileLink
-              key={'GoodGameRuler'}
-              username={'GoodGameRuler'}
-              imageUrl={'/images/udit.jpeg'}
-            />
-            <ProfileLink
-              key={'gitparth12'}
-              username={'gitparth12'}
-              imageUrl={'/images/parth.jpeg'}
-            />
-            <ProfileLink
-              key={'devanshimirchandani'}
-              username={'devanshimirchandani'}
-              imageUrl={'/images/devu.jpeg'}
-            />
-            <ProfileLink
-              key={'antrikshdhandv'}
-              username={'antrikshdhand'}
-              imageUrl={'/images/antriksh.png'}
-            />
-        </div>
-      </div>
-      <Divider />
-
-      <div className="contentContainer">
         <h2 className="subheading"> Keen to Contribute? </h2>
+        <div className="body aboutUs">See the full team on GitHub!</div>
         <div className="profileContainer">
           <ProfileLink
             key={'github'}

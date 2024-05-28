a48b9d78a347e378fb60b899806f5a745bc3dfcb
Udit <udit.samant@gmail.com>
2024-05-28 12:46:28 +0900

    Change layout and naming final

---
+++ app/navBarLogin.jsx
@@ -0,0 +1,31 @@
+
+export function navBarLogin() {
+    return (
+        <div className="userLoginDetails">
+            <div className="loginInfoContainer" id="scrollBot">
+                <div className="hiddenButtons">
+                    <button className="navBarButton" onClick={() => {console.log("Working")}}>
+                        <i className="material-icons">auto_stories</i>&nbsp;&nbsp;
+                        Settings
+                    </button>
+                    <button className="navBarButton" onClick={() => {console.log("Working")}}>
+                        <i className="material-icons">auto_stories</i>&nbsp;&nbsp;
+                        Sign In
+                    </button>
+                    <button className="navBarButton" onClick={() => {console.log("Working")}}>
+                        <i className="material-icons">auto_stories</i>&nbsp;&nbsp;
+                        Register
+                    </button>
+                    <hr className="solid" />
+                </div>
+                <div className="buttonColumn" id="anchor">
+                    <i className="material-icons">account_circle</i>&nbsp;&nbsp;
+                    <div className="buttonRow">
+                        Guest User <br />
+                        <small>or Sign in here.</small>
+                    </div> <br />
+                </div>
+            </div>
+        </div>
+    )
+}
